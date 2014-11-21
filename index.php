c781f6467e534f25eb32c74a8d88226a09520ed1
Brian Turley <turleybw@gmail.com>
2014-11-21 16:57:03 -0700

    First Projects Commit from local host
    Created hover sidebar nav for all pages

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+	<title>Brian Turley's code projects</title>
+<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+	<div id="content">
+	<h1>Welcome to my projects</h1>
+	<p>thaisihgiohaig agiaigiahgihaighoiahgihaighiahg o agohiahg oiahg oiahg iahgi aighiagihag ihai g</p>
+	<?php include 'bt-menu.php';?>
+
+	</div>
+
+</body>
+</html>
